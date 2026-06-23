32bc1ed38868d75a23a09e26487f493948b7d94d
aherlaud <aherlaud@student.42.fr>
2026-06-23 16:47:14 +0200

    feat(data filtering): filter possible options of disease and synptoms
    - src/backend/script/test.py: adapt script to gain access to possible disease and direct the user to possible other key symptoms

--- src/backend/script/test.py
+++ src/backend/script/test.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import json
 
+sexe = "Femme"
+Age = "70 - 100"
+motif = "dorsalgie haute"
+
 df = pd.read_csv("data/sympto_patho_test.csv")
 df["GROUPE"] = df["GROUPE"].ffill()
 df = df.set_index(["CLASSIFICATION","SYMPTÔME SPÉ"])
@@ -9,5 +13,16 @@ del df["GROUPE"]
 
 df = df.apply(pd.to_numeric, errors="coerce")
 
-s = df.loc[("Age", "35 - 55")]
-print(s[s >= 0.5])
+s_sex = df.loc[("Sexe", sexe)]
+s_age = df.loc[("Age", Age)]
+s_motif = df.loc[("Motif", motif)]
+
+mask = (s_sex > 0) & (s_age > 0) & (s_motif > 0)
+list_conv = s_age.index[mask].tolist()
+
+motifs = df.loc["Motif"]
+motifs = motifs[motifs.index != motif][list_conv]
+motifs_index = motifs.index
+result = motifs[(motifs > 0).sum(axis=1) == 1].index.tolist()
+print(df[list_conv])
+print(result)
